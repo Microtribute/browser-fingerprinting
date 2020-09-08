af1867bca6b43fcf8a23589f3f144c810ebd068c
Microtribute <microtribute@github.io>
2020-09-08 17:07:58 +0100

    fingerprint.js backup

---
+++ base64.php
@@ -0,0 +1,5 @@
+<?php
+$token = '6f4NaUjYbVy5IApMLpiYx/JDUZ3V98ilYCON4vsVghk2p50zvQ8eYI4Bv30yAZwp3/T4NbV9Bm4kzr7RvKFaeE/fN3tQHecec+ZaeWAjqPrNM3etiwOgUVsASlZxUW5i0jWsokvJbS/U/I5qqw==';
+
+var_dump(base64_decode($token, true));
+
